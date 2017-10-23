647213a2196f36aab135c43325bb05226e66cc65
tlu200 <tlu200@users.noreply.github.com>
2017-10-23 16:14:49 -0700

    Add reducers

---
+++ reducers/index.js
@@ -0,0 +1,29 @@
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions';
+import { createReducer, updateObject } from './reducerUtilities';
+
+function receiveDecks(state, action) {
+  return action.decks;
+}
+
+function addDeck(state, action) {
+  return {
+    ...state,
+    ...{
+      title: action.title,
+      questions: []
+    }
+  }
+}
+
+function addCard(state, action) {
+  const deck = state[action.title] || { title, questions: []};
+  return updateObject(state, {
+    [action.title]: deck.push(action.card)
+  });
+}
+
+export default createReducer({}, {
+  [RECEIVE_DECKS]: receiveDecks,
+  [ADD_DECK]: addDeck,
+  [ADD_CARD]: addCard
+})
